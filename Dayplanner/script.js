207f7468a4b1cccb0436718957c03580951a7289
Eric Rosario <steven.e.rosario@gmail.com>
2020-09-22 14:41:50 -0700

    ?

---
+++ Dayplanner/script.js
@@ -0,0 +1,5 @@
+moment().format('MMMM Do YYYY, h:mm:ss a'); // September 16th 2020, 6:00:18 pm
+moment().format('dddd');                    // Wednesday
+moment().format("MMM Do YY");               // Sep 16th 20
+moment().format('YYYY [escaped] YYYY');     // 2020 escaped 2020
+moment().format();                          // 2020-09-16T18:00:18-07:00
